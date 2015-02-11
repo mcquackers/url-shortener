42665f2c43847ddad5780013af4e2a1ec07fa3a3
Brian McQueen <brian.d.mcqueen@gmail.com>
2015-02-11 13:33:56 -0500

    Add serverside URL validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,13 +1,24 @@
 class Link < ActiveRecord::Base
+  validate :is_a_valid_url
+
   belongs_to :user
 
-  after_save :generate_slug
+  after_create :generate_slug
 
   private
 
-  def generate_slug
-    unless slug
-      slug = id.to_s(36)
+  def is_a_valid_url
+    http_request = Net::HTTP.new(url)
+    begin
+      http_response = http_request.request_get("/")
+      http.response.is_a?(Net::HTTPSuccess) || http_response.code == "200"
+    rescue ErrNo::ECONNREFUSED
+      errors.add(:url, "Must be a valid non-directing URL")
     end
+  end
+
+  def generate_slug
+    self.slug = self.id.to_s(36)
+    self.save
   end
 end
