418e81f96654dd60683048505e3286de302ea91c
Brian McQueen <brian.d.mcqueen@gmail.com>
2015-02-11 14:09:52 -0500

    Create link-inforecord relationship; amend validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,6 +6,7 @@ class Link < ActiveRecord::Base
   has_one :info_record
 
   after_create :generate_slug
+  after_create :generate_info_record
 
   private
 
@@ -13,8 +14,9 @@ class Link < ActiveRecord::Base
     http_request = Net::HTTP.new(url)
     begin
       http_response = http_request.request_get("/")
-      http.response.is_a?(Net::HTTPSuccess) || http_response.code == "200"
-    rescue ErrNo::ECONNREFUSED
+      puts http_response.code
+      http_response.code == "200"
+    rescue
       errors.add(:url, "Must be a valid non-directing URL")
     end
   end
@@ -22,5 +24,15 @@ class Link < ActiveRecord::Base
   def generate_slug
     self.slug = self.id.to_s(36)
     self.save
+  end
+
+  def generate_info_record
+    self.info_record = InfoRecord.create(
+      user_id: self.user_id,
+      link_id: self.id,
+      url: self.url,
+      slug: self.slug,
+      disabled: self.disabled
+      )
   end
 end
